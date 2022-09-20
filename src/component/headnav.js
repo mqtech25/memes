8f437f7d6a637c42307c4fe56a7a44b07ebce096
Muhammad Qasim <mqtech25@gmail.com>
2022-09-21 00:55:10 +0500

    meme generation app respository

---
+++ src/component/headnav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Nav() {
+    return(
+        <div className="App-nav container">
+            <div className="brand">
+                <img src="./img/trollFace.png" className="App-logo"></img>
+                Meme Genertation
+            </div>
+            <div className="nav">
+                React Project
+            </div>
+        </div>
+    )
+}
